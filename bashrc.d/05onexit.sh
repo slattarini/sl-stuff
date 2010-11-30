2cad2c352e79a1dca265b64316654602b7af220a
Stefano Lattarini <stefano.lattarini@gmail.com>
2010-11-30 15:18:11 +0100

    exit trap: cosmetics

--- bashrc.d/05onexit.sh
+++ bashrc.d/05onexit.sh
@@ -6,22 +6,23 @@
 declare -a EXEC_ON_EXIT_ACTIONS=(":")
 
 _exit_trap() {
-    local i _exit_action _exit_status=$?
-    trap - EXIT
     PS1=''
     unset PROMPT_COMMAND
-    # execute planned cleanup actions
+    local i _exit_action _exit_status=$?
+    trap - EXIT
+    # Execute planned cleanup actions in the proper order.
     for _exit_action in "${EXEC_ON_EXIT_ACTIONS[@]}"; do
         eval "${_exit_action}" || _exit_status=$?
     done
-    # clear screen
+    # Really clear screen, so that even page-up commands cannot show
+    # it anymore.
     if [ 1 -eq ${SHLVL} ] && { IsHost bigio || IsHost bplab; }; then
         for ((i = 0; i < 5000; i++)); do
             echo
         done
     fi
-    # clear history file
-    true > "${HISTFILE:-"$HOME/.bash_history"}" || _exit_status=1
+    # Clear history file.
+    : > "${HISTFILE-"$HOME/.bash_history"}" || _exit_status=1
     # Reset terminal defaults.
     tput reset
     exit ${_exit_status}
