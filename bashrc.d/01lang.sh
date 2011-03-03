38558cfa8a40d9f6bcb97d2171a5b2db704c45a4
Stefano Lattarini <stefano.lattarini@gmail.com>
2011-03-03 21:07:32 +0100

    make l10n-related functions unconditionally defined

--- bashrc.d/01lang.sh
+++ bashrc.d/01lang.sh
@@ -2,50 +2,42 @@
 
 # I18N and L10N
 
-if [[ $SYSTEM_UNAME != freebsd ]] && { IsHost bigio || IsHost bplab; }; then
+W nlang || {
+    mwarn "Missing program \`nlang'" >&2
+    mwarn "Fancy localization functions won't be available" >&2
+    return $SUCCESS
+}
 
-    W nlang || {
-        fwarn "Missing program \`nlang'" >&2
-        return $FAILURE
-    }
+savelang() {
+    _restore_lang_cmd=$(nlang)
+}
 
-    savelang() {
-        _restore_lang_cmd=$(nlang)
-    }
+resetlang() {
+    eval "${_restore_lang_cmd}" && _restore_lang_cmd=""
+}
 
-    resetlang() {
-        eval "${_restore_lang_cmd}" && _restore_lang_cmd=""
-    }
+setlang() {
+    eval $(nlang "$@")
+    export LC_COLLATE=C
+    export LC_NUMERIC=C
+}
 
-    setlang() {
-        eval $(nlang "$@")
-        export LC_COLLATE=C
-        export LC_NUMERIC=C
-    }
+clearlang() {
+    eval $(nlang '')
+}
 
-    clearlang() {
-        eval $(nlang '')
-    }
+speak() {
+    case "${1-}" in
+        crucco|de|deutsch)  set de_DE.UTF-8;;
+        mafia|it|italiano)  set it_IT.UTF-8;;
+         limey|en|english)  set en_GB.UTF-8;;
+            yankee|us|usa)  set en_US.UTF-8;;
+              fr|français)  set fr_FR.UTF-8;;
+    esac
+    setlang "$1"
+}
 
-    speak() {
-        case "${1-}" in
-            crucco|de|deutsch)  set de_DE.UTF-8;;
-            mafia|it|italiano)  set it_IT.UTF-8;;
-             limey|en|english)  set en_GB.UTF-8;;
-                yankee|us|usa)  set en_US.UTF-8;;
-                  fr|français)  set fr_FR.UTF-8;;
-        esac
-        setlang "$1"
-    }
-
-    savelang
-    setlang -f it_IT.UTF-8
-
-else
-
-    clearlang() { :; }
-    export LANG=C LANGUAGE=C LC_ALL=C
-
-fi
+savelang
+setlang -f it_IT.UTF-8
 
 # vim: ft=sh ts=4 sw=4 et
