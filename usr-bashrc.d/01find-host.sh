45e87cb0275ba095ce200904149628fb329b8884
Stefano Lattarini <stefano.lattarini@gmail.com>
2011-10-03 14:12:09 +0200

    01find-host.sh: be user-agnostic

--- usr-bashrc.d/01find-host.sh
+++ usr-bashrc.d/01find-host.sh
@@ -1,25 +1,17 @@
 # -*- bash -*-
 
-# Simplified name of the current host.
-case "$(id -un)@$(uname -n)" in
-    (stefano@bigio|stefano@bigio.localdomain)
-        hostname=bigio;;
-    (ste496@bpserv?(2)?(.*))
-        hostname=bpserv;;
-    (ste496@bplab?(.*))
-        hostname=bplab;;
-    (latta@freddy)
-        hostname=freddy;;
-    (*)
-        mwarn "cannot determine simplified name of current system"
-        hostname=UNKNOWN;;
+# Name of the current host, "normalized" if possible.
+hostname=$(uname -n)
+case $hostname in
+    (bigio|bigio.localdomain)   hostname=bigio  ;;
+    (bpserv?(2)?(.*))           hostname=bpserv ;;
+    (bplab?(.*))                hostname=bplab  ;;
+    (latta@freddy)              hostname=freddy ;;
 esac
-
-IsHost() {
-    [[ $hostname == $1 ]]
-}
-
 declare -r hostname
+
+IsHost() { [[ $hostname == $1 ]]; }
+
 declare -rf IsHost
 
 # vim: ft=sh ts=4 sw=4 et
