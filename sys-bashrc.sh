6fefd687cf855a2be4d44a8c083052a04f972222
Stefano Lattarini <stefano.lattarini@gmail.com>
2011-10-03 08:51:02 +0200

    sys-bashrc.sh: fixlets and improvements in comments

--- sys-bashrc.sh
+++ sys-bashrc.sh
@@ -1,15 +1,16 @@
 # -*- bash -*-
 # General-purpose .bashrc file for interactive bash(1) shells.
 #
-# This is meant to be installed at system level if possible, and to be
-# used by all the users on a system (by being *explicitly* sourced by
-# their ~/.bashrc).  It can also be installed in the home directory,
-# though, fore example on systems where one lacks root access.
+# This file is meant to be installed at global, system level if
+# possible, and to be used by all the users of the system (by being
+# *explicitly* sourced by their ~/.bashrc).  It can also be installed
+# in the home directory, though, fore example on systems where one
+# lacks root access.
 #
 # This file is *not* a substitute for /etc/bash.bashrc; it rather strives
-# to complement that file, by defining useful extensions, function and
-# alias that are either system-agnotic, or tries to adaptively work on a
-# quite large range of systems.  OTOH, /etc/bash.bashrc is usually meant
+# to complement that file, by defining useful extensions, functions and
+# aliases that are either system-agnostic, or tries to adaptively work on
+# a quite large range of systems.  OTOH, /etc/bash.bashrc is usually meant
 # to be tailored for, or even tied to, a specific system or machine.
 
 # Explicitly read system-wide profile (needed in case we're not a login
