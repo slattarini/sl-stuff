398b1d4f50a03f203d86fb83a214fc84af329e0d
Stefano Lattarini <stefano.lattarini@gmail.com>
2011-10-03 09:44:09 +0200

    merge bashrc.sh and sys-bashrc.sh

--- bashrc.sh
+++ bashrc.sh
@@ -1,4 +1,17 @@
 #-*- bash -*-
+# General-purpose .bashrc file for interactive bash(1) shells.
+#
+# This file is meant to be installed at global, system level if
+# possible, and to be used by all the users of the system (by being
+# *explicitly* sourced by their ~/.bashrc).  It can also be installed
+# in the home directory, though, fore example on systems where one
+# lacks root access.
+#
+# This file is *not* a substitute for /etc/bash.bashrc; it rather strives
+# to complement that file, by defining useful extensions, functions and
+# aliases that are either system-agnostic, or tries to adaptively work on
+# a quite large range of systems.  OTOH, /etc/bash.bashrc is usually meant
+# to be tailored for, or even tied to, a specific system or machine.
 
 # Shell must be interactive.
 case "$-" in *i*) ;; *) return 0;; esac
@@ -24,11 +37,19 @@ case $BASH_VERSION in
         return 0;;
 esac
 
-PS1='\u@\h[bash-\v]$ '
-
 # Protect against multiple inclusion
 [ x${BASHSHRC_INCLUDED+"set"} = x"set" ] && return 0
 readonly BASHSHRC_INCLUDED=1
+
+# Explicitly read system-wide profile (needed in case we're not a login
+# shell).
+. /etc/profile
+
+# Check the window size after each command and, if necessary, update the
+# values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+PS1='\u@\h[bash-\v]$ '
 
 BASHRC_DIR="$HOME/.bashrc.d"
 
