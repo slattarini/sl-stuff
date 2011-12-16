027caf5aafe74f11ab36486cfdfdbc4ce8563a73
Stefano Lattarini <stefano.lattarini@gmail.com>
2011-12-17 00:08:41 +0100

    golang: adapt to new possible location of go repo

--- usr-bashrc.d/84golang.sh
+++ usr-bashrc.d/84golang.sh
@@ -1,8 +1,8 @@
 # -*- bash -*-
 
-if [ -d $HOME/go ]; then
+if [ -d $HOME/src/go ]; then
     if [[ $SYSTEM_UNAME == ?(freebsd|linux) ]]; then
-        export GOROOT=$HOME/go
+        export GOROOT=$HOME/src/go
         export GOBIN=$GOROOT/bin
         export GOOS=$SYSTEM_UNAME
         export GOARCH=386
