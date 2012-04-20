be5b91e84bfc76f5d471e5d73f069d56edf87914
Stefano Lattarini <stefano.lattarini@gmail.com>
2012-04-20 11:44:12 +0200

    remove definitions of few now-useless env vars
    Signed-off-by: Stefano Lattarini <stefano.lattarini@gmail.com>

--- bashrc.d/70S-miscvars.sh
+++ bashrc.d/70S-miscvars.sh
@@ -22,16 +22,6 @@ W lesspipe && eval "$(lesspipe)"
 # Override defaults for autoconf-generated configure scripts.
 test -f $HOME/config.site && export CONFIG_SITE=$HOME/config.site
 
-# W3C (X)HTML/CSS validator.
-export HTML_VALIDATOR_URL=http://validator.w3.org/check
-export CSS_VALIDATOR_URL=http://jigsaw.w3.org/css-validator/validator
-
-# Device used by cdda2wav (with `cooked_ioctl' interface).
-IsHost bigio && export CDDA_DEVICE='/dev/dvd'
-
-# Used by ~/bin/svo.
-IsHost bigio && export SVN_BASEURL="https://svn.bigio/svn"
-
 # Editors used by whed(1) and its links.
 case $hostname in
   bpserv|freddy)
@@ -47,9 +37,9 @@ export WH_VIM WH_GVIM
 
 # Root directory of the sandboxed testing environments used by the
 # `test-in-sandbox' script.
-export SANDBOX_TESTING_ENVIRONMENTS=$HOME/src/sandboxed-testing/sandboxes/$hostname-$SYSTEM_UNAME
+#export SANDBOX_TESTING_ENVIRONMENTS=$HOME/src/sandboxed-testing/sandboxes/$hostname-$SYSTEM_UNAME
 # Directory of the plugins used by the sandboxed testing environments.
-export SANDBOX_TESTING_PLUGINS=$HOME/src/sandboxed-testing/test-in-sandbox/plugins
+#export SANDBOX_TESTING_PLUGINS=$HOME/src/sandboxed-testing/test-in-sandbox/plugins
 
 # Trash directory used by my del(1) utility.
 if IsHost freddy; then
