59992f623d28dd96fe93aeeb447c0695a3d4bf0b
Stefano Lattarini <stefano.lattarini@gmail.com>
2012-05-25 12:41:32 +0200

    add support for bledding-edge devel tools
    Signed-off-by: Stefano Lattarini <stefano.lattarini@gmail.com>

---
+++ bashrc.d/02S-bleeding.sh
@@ -0,0 +1,11 @@
+# -*- bash -*-
+
+# Support for bleeding-edge development tools.
+# This must go after any initialization of PATH, INFOPATH, MANPATH
+# and ACLOCAL_PATH.
+
+if test x"$shiny_new_tools" = x"yes"; then
+  . bleeding
+fi
+
+# vim: ft=sh ts=4 sw=4 et
