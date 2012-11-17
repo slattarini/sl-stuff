417b18d9e34941b61e604953c7a13d4993916736
Stefano Lattarini <stefano.lattarini@gmail.com>
2012-11-17 17:11:18 +0100

    gpg: export GPG_TTY unconditionally (required by pinentry-courses)
    Signed-off-by: Stefano Lattarini <stefano.lattarini@gmail.com>

--- bashrc.d/40S-gpg-agent.sh
+++ bashrc.d/40S-gpg-agent.sh
@@ -7,6 +7,10 @@ elif [[ ${GPG_AGENT_INFO+set} == set ]]; then
     # gpg-agent machinery has been already started
     mwarn "Not starting gpg-agent machinery: it is already active"
     mwarn "GPG_AGENT_INFO=$GPG_AGENT_INFO"
+    # This is required, if we open an xterm in an X session already
+    # wrapped by gpg-agent.
+    export GPG_TTY=$(tty)
+    # Nothing more to do.
     return $SUCCESS
 fi
 
@@ -20,8 +24,6 @@ eval "$(gpg-agent --daemon --sh)" \
   && GPG_AGENT_PID=${GPG_AGENT_PID%:*} \
   && export GPG_AGENT_PID GPG_AGENT_INFO \
   && atexit 'kill_gpg_agent' \
-  || { kill_gpg_agent; return $FAILURE; } \
-  && export GPG_TTY=$(tty) \
   || { kill_gpg_agent; return $FAILURE; }
 
 # vim: ft=sh ts=4 sw=4 et
