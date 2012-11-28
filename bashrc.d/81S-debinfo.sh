088d40583cc6a399bbd471d78981cfd2a8ebbb37
Stefano Lattarini <stefano.lattarini@gmail.com>
2012-11-28 14:04:33 +0100

    debinfo: activate only on Debian
    Signed-off-by: Stefano Lattarini <stefano.lattarini@gmail.com>

--- bashrc.d/81S-debinfo.sh
+++ bashrc.d/81S-debinfo.sh
@@ -1,6 +1,8 @@
 # -*- bash -*-
-
 # Info used when creating Debian packages.
+
+[ -f /etc/debian_version ] || return $SUCCESS
+
 DEBEMAIL=stefano.lattarini@gmail.com
 DEBFULLNAME="Stefano Lattarini"
 
