2f1170ccc087c34f0606694ecdf39d9c34a15e82
Stefano Lattarini <stefano.lattarini@gmail.com>
2012-11-28 13:53:21 +0100

    man-path: remove useless stderr redirection, and style fixes
    Signed-off-by: Stefano Lattarini <stefano.lattarini@gmail.com>

--- bashrc.d/01S-manpath.sh
+++ bashrc.d/01S-manpath.sh
@@ -1,12 +1,19 @@
 #-*- bash -*-
-
 # Set MANPATH variable (for search of man pages).
 
-MANPATH=""
+MANPATH=''
 add_to_path -B -p 'MANPATH' \
-  /usr/man /usr/share/man /usr/local/man /opt/man \
-  /opt/java/sun-java/man "$HOME"/man \
-    2>/dev/null
-[ -n "$MANPATH" ] && export MANPATH || unset MANPATH
+  /usr/man \
+  /usr/share/man \
+  /usr/local/man \
+  /opt/man \
+  /opt/java/sun-java/man \
+  "$HOME"/man
+
+if [ -n "$MANPATH" ]; then
+    export MANPATH
+else
+    unset MANPATH
+fi
 
 # vim: ft=sh ts=4 sw=4 et
