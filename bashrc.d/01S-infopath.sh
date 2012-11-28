15ac52f037a2932e21f91bcb0393db8ade3ba683
Stefano Lattarini <stefano.lattarini@gmail.com>
2012-11-28 13:51:59 +0100

    info-path: style fixes
    Signed-off-by: Stefano Lattarini <stefano.lattarini@gmail.com>

--- bashrc.d/01S-infopath.sh
+++ bashrc.d/01S-infopath.sh
@@ -2,15 +2,24 @@
 # Set INFOPATH variable (for search of documentation in info format).
 
 if W info; then
-    INFOPATH=""
+
+    INFOPATH=''
+
     add_to_path -B -p 'INFOPATH' \
-      /usr/info /usr/share/info /usr/local/info /usr/local/share/info \
-      /opt/info "$HOME"/info "$HOME"/share/info
+      /usr/info \
+      /usr/share/info \
+      /usr/local/info \
+      /usr/local/share/info \
+      /opt/info \
+      "$HOME"/info \
+      "$HOME"/share/info
+
     if [ -n "$INFOPATH" ]; then
         export INFOPATH
     else
         unset INFOPATH
     fi
+
 else
     mwarn "program info(1) not found, thus simply unsetting INFOPATH"
     unset INFOPATH
