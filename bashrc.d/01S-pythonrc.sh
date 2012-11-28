0e65b0d5d0fc755dfa25995f831e8e44ab5acaad
Stefano Lattarini <stefano.lattarini@gmail.com>
2012-11-28 13:53:55 +0100

    pythonrc: remove extra quoting
    Signed-off-by: Stefano Lattarini <stefano.lattarini@gmail.com>

--- bashrc.d/01S-pythonrc.sh
+++ bashrc.d/01S-pythonrc.sh
@@ -1,8 +1,8 @@
 # -*- bash -*-
 
 # Set file executed at startup by interactive python interpreter.
-if [ -f "$HOME/.pythonrc" ]; then
-  export PYTHONSTARTUP="$HOME/.pythonrc"
+if [[ -f $HOME/.pythonrc ]]; then
+  export PYTHONSTARTUP=$HOME/.pythonrc
 fi
 
 # vim: ft=sh ts=4 sw=4 et
