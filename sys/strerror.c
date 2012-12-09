625290d70bd938638cd2384a6315114a811ef25d
Stefano Lattarini <stefano.lattarini@gmail.com>
2012-12-09 10:08:55 +0100

    strerror: avoid warning due to 'undeclared atoi()'
    Signed-off-by: Stefano Lattarini <stefano.lattarini@gmail.com>

--- sys/strerror.c
+++ sys/strerror.c
@@ -1,7 +1,8 @@
 /* Take an errno number as input and print the associated error
    message on stderr. */
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h> /* for atoi */
+#include <string.h> /* for strerror */
 #include <errno.h>
 int main (int argc, char **argv)
 {
