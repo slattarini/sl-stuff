26b7a3a1e0563b579a85f69e098183ff3130b550
Stefano Lattarini <stefano.lattarini@gmail.com>
2012-12-22 09:56:27 +0100

    strerror: error out on missing arguments
    Signed-off-by: Stefano Lattarini <stefano.lattarini@gmail.com>

--- sys/strerror.c
+++ sys/strerror.c
@@ -7,6 +7,10 @@
 int main (int argc, char **argv)
 {
     int errnum;
+    if (argc == 1) {
+      fprintf (stderr, "%s: missing argument\n", argv[0]);
+      return 2;
+    }
     while (--argc > 0) {
         errnum = atoi(*++argv);
         printf ("strerror(%d) = %s\n", errnum, strerror (errnum));
