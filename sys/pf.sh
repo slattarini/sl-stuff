fd03e3a8bf580d67dffcb71467a046f011f552db
Stefano Lattarini <stefano.lattarini@gmail.com>
2012-12-22 13:10:34 +0100

    pf: refactorings and style changes
    Signed-off-by: Stefano Lattarini <stefano.lattarini@gmail.com>

--- sys/pf.sh
+++ sys/pf.sh
@@ -6,56 +6,61 @@
 set -u
 
 progname=${0##*/}
-field=0
-usage="\
-usage: $progname [--help|-h] [-F FIELD-SEPARATOR] [--] FIELD-NUM
-       [FILE-1 ... FILE-n]"
-help="$usage
-\"FILED-NUM = 0\" means all fields."
+
+print_usage ()
+{
+  echo "Usage: $progname [--help] [-F FIELD-SEPARATOR] [--]"
+  echo "       FIELD-NUMBER [FILE-1 ... FILE-n]"
+}
+
+print_help ()
+{
+  print_usage
+  echo "\"FIELD-NUM = 0\" means all fields."
+}
 
 usage_error ()
 {
-    [ $# -gt 0 ] && echo "$progname: $*" >&2
-    echo "$usage" >&2
-    exit 2
+  [ $# -gt 0 ] && echo "$progname: $*" >&2
+  print_usage >&2
+  exit 2
 }
 
 awk_field_separator=' '
-
 while [ $# -gt 0 ]; do
-    case "$1" in
-        --help|-help|-h|-\?)
-            echo "$help"
-            exit
-            ;;
-        -F)
-            [ $# -eq 1 ] && usage_error "Option  '$1' requires an argument"
-            awk_field_separator="$2"
-            shift
-            ;;
-        -F*)
-            awk_field_separator=`echo "X$1" | sed 's/^X-F//'`
-            ;;
-        --)
-            shift
-            break
-            ;;
-        -*)
-            usage_error "'$1': Unknown option"
-            ;;
-        *)
-            break
-            ;;
-    esac
-    shift
+  case $1 in
+    --help)
+      print_help; exit $?
+      ;;
+    -F)
+      if [ $# -eq 1 ]; then
+        usage_error "'$1': option requires an argument"
+      else
+        awk_field_separator=$2
+        shift
+      fi
+      ;;
+    -F*)
+      awk_field_separator=${1##-F};;
+    --)
+      shift; break;;
+    -*)
+      usage_error "'$1': invalid option";;
+    *)
+      break
+      ;;
+  esac
+  shift
 done
 
-[ $# -gt 0 ] || usage_error "No field specified"
-
-[ 0 -le "$1" ] >/dev/null 2>&1 || \
-  usage_error "'$1': Not a non-negative integer"
-
-field=$1 && shift
+if [ $# -gt 0 ]; then
+  usage_error "no field specified"
+elif ! [ 0 -le "$1" ] >/dev/null 2>&1; then
+  usage_error "'$1': not a non-negative integer"
+else
+  field=$1
+  shift
+fi
 
 exec awk -F"$awk_field_separator" "{ print \$$field; }" ${1+"$@"}
 
