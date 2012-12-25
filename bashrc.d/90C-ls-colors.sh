c0288936a901d5326adac50e58a03107d957c0b4
Stefano Lattarini <stefano.lattarini@gmail.com>
2012-12-25 21:22:49 +0100

    ls-colors: look for both ~/.dircolors and ~/.dir_colors
    Signed-off-by: Stefano Lattarini <stefano.lattarini@gmail.com>

--- bashrc.d/90C-ls-colors.sh
+++ bashrc.d/90C-ls-colors.sh
@@ -1,8 +1,14 @@
 # -*- bash -*-
-
 # Color scheme for ls(1).
-if [[ ${TERM-dumb} != dumb && -f $HOME/.dir_colors ]] && W dircolors; then
-    eval $(dircolors -b "$HOME/.dir_colors")
-fi
+
+[[ ${TERM-dumb} != dumb ]] && W dircolors || return $SUCCESS
+
+for f in ~/.dircolors ~/.dir_colors; do
+  if [[ -f $f ]]; then
+    eval $(dircolors -b "$f")
+    break
+  fi
+done
+unset f
 
 # vim: ft=sh ts=4 sw=4 et
