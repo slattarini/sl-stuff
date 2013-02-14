b4a131fb524f0e3716284546528b76cc7d8b5d69
Stefano Lattarini <stefano.lattarini@gmail.com>
2013-02-14 15:20:26 +0100

    editor: add definition of GIT_EDITOR
    Signed-off-by: Stefano Lattarini <stefano.lattarini@gmail.com>

--- bash-init/bashrc.d/10C-editor.sh
+++ bash-init/bashrc.d/10C-editor.sh
@@ -11,7 +11,10 @@ if test -z "$EDITOR"; then
 fi
 export EDITOR
 
-# Editor used by tex/latex.
+# Editor used by TeX/LaTeX.
 export TEXEDIT="$EDITOR +%d %s"
+
+# Editor used by Git.
+export GIT_EDITOR=$EDITOR
 
 # vim: ft=sh et ts=4 sw=4
