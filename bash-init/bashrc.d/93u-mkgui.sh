9d21342eedd17262a4579c91baecae40eafcc35d
Stefano Lattarini <stefano.lattarini@gmail.com>
2013-03-06 13:41:53 +0100

    bashrc: don't GUIfy ddd and bluej, I don't truly use them
    Signed-off-by: Stefano Lattarini <stefano.lattarini@gmail.com>

--- bash-init/bashrc.d/93u-mkgui.sh
+++ bash-init/bashrc.d/93u-mkgui.sh
@@ -105,8 +105,8 @@ MakeGUI ()
 
 MakeGUI \
     firefox libreoffice pidgin vuze easytag emacs \
-    gitk qgit hgview xpdf xdvi gv ddd iceweasel icedove \
-    smalltalk bluej cssed civ freeciv heretic alsaplayer
+    gitk qgit hgview xpdf xdvi gv iceweasel icedove \
+    smalltalk cssed civ freeciv heretic alsaplayer
 
 MakeGUI -w aoss -- clanbomber bomberclone
 
