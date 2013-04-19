2cdb615a547d146395aabee4e0f28fff2a54dd0d
Stefano Lattarini <stefano.lattarini@gmail.com>
2013-04-19 12:06:25 +0200

    bash init: completion: more namespace safety
    Signed-off-by: Stefano Lattarini <stefano.lattarini@gmail.com>

--- bash-init/bashrc.d/99C-bash-completion.sh
+++ bash-init/bashrc.d/99C-bash-completion.sh
@@ -9,32 +9,32 @@
 # aliases and/or functions defined in the other shell initialization files.
 #
 
-if [[ -z $BASH_COMPLETION ]]; then
+if [[ -z $BASH_COMPLETION_SL ]]; then
     for d in $HOME/etc /usr/local/etc /etc /usr/share/bash-completion; do
         if test -f $d/bash_completion; then
-            BASH_COMPLETION=$d/bash_completion
+            BASH_COMPLETION_SL=$d/bash_completion
             break
         fi
     done
 fi
 
-if [[ -z $BASH_COMPLETION ]]; then
+if [[ -z $BASH_COMPLETION_SL ]]; then
     mwarn "  no bash completion file could be found"
     mwarn "  Bash advanced completion features won't be available."
     return $SUCCESS
 fi
 
-if [[ ! -f $BASH_COMPLETION || ! -r $BASH_COMPLETION ]]; then
-    mwarn "  expected bash completion file '$BASH_COMPLETION"
+if [[ ! -f $BASH_COMPLETION_SL || ! -r $BASH_COMPLETION_SL ]]; then
+    mwarn "  expected bash completion file '$BASH_COMPLETION_SL"
     mwarn "  does not exist or is not a regular readable file."
     mwarn "  Bash advanced completion features won't be available."
     return $SUCCESS
 fi
 
-. "$BASH_COMPLETION"
+. "$BASH_COMPLETION_SL"
 if (($? != 0)); then
     mwarn "some errors occurred while loading bash completion file" \
-          "'$BASH_COMPLETION'"
+          "'$BASH_COMPLETION_SL'"
     mwarn "Some bash advanced completion features may not be available."
     return $SUCCESS
 fi
