1b7684907eaeac6b7ea11b3cf8e7604cdab86e5c
Stefano Lattarini <stefano.lattarini@gmail.com>
2013-11-03 00:22:11 +0000

    bash-init: more i18n fixes
    - be atomic in setlang()
    - really catch errors (attempt in previous commit was borked)
    - reformat existing code to use 2-spaces indentation
    
    Signed-off-by: Stefano Lattarini <stefano.lattarini@gmail.com>

--- bash-init/bashrc.d/01u-lang.sh
+++ bash-init/bashrc.d/01u-lang.sh
@@ -8,37 +8,43 @@ locale -a &>/dev/null || return $SUCCESS
 
 setlang ()
 {
-    local x rc=0
-    for x in $(locale); do
-        eval "${x%%=*}=$1" || rc=1
-    done
-    case $1 in C) export LC_ALL=C;; *) unset LC_ALL;; esac || rc=1
-    export LC_COLLATE=C && export LC_NUMERIC=C || rc=1
-    # Apparently, perl is very good at detecting broken locales.
-    (locale; perl -e 1) 2>&1 >/dev/null | grep . >&2 && rc=1
-    return $rc
+  local vardef errs
+  # Use dirty tricks to try to make language setting atomic.
+  if ! ((_avoid_setlang_checks)); then
+    errs=$(_avoid_setlang_checks=1; setlang "$1" 2>&1) || return 1
+    [[ -z $errs ]] || { xecho "$errs" >&2; return 1; }
+  fi
+  declare -r rc=0
+  for x in $(locale); do
+    eval "${x%%=*}=$1" || rc=1
+  done
+  case $1 in C) export LC_ALL=C;; *) unset LC_ALL;; esac || rc=1
+  export LC_COLLATE=C && export LC_NUMERIC=C || rc=1
+  return $rc
 }
 
 clearlang ()
 {
-    setlang "C"
+  setlang "C"
 }
 
 speak ()
 {
-    case ${1-} in
-        en|USA)     set en_US.UTF-8;;
-        uk|british) set en_GB.UTF-8;;
-        de|german)  set de_DE.UTF-8;;
-        fr|french)  set fr_FR.UTF-8;;
-        it|italian) set it_IT.UTF-8;;
-    esac
-    setlang "$1"
+  case ${1-} in
+    en|USA)     set en_US.UTF-8;;
+    uk|british) set en_GB.UTF-8;;
+    de|german)  set de_DE.UTF-8;;
+    fr|french)  set fr_FR.UTF-8;;
+    it|italian) set it_IT.UTF-8;;
+  esac
+  setlang "$1"
 }
 
 setlang en_US.UTF-8 || {
-    mwarn "failed setting locale to en_US.UTF-8, falling back to C locale"
-    clearlang
+  mwarn "failed setting locale to en_US.UTF-8, falling back to C locale"
+  clearlang
 }
+
+return 0
 
 # vim: ft=sh ts=4 sw=4 et
